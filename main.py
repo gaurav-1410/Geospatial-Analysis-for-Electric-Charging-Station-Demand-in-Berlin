f603e0c350650d86b94218cb028d6491a63862c7
gaurav-1410 <gp141022@gmail.com>
2025-01-06 01:24:13 +0100

    Updated Code

--- main.py
+++ main.py
@@ -13,32 +13,9 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 
 firebase_creds = st.secrets["firebase"]
-
-# Initialize Firebase with the secrets
-# cred = credentials.Certificate({
-#     "type": "service_account",
-#     "project_id": firebase_creds["project_id"],
-#     "private_key_id": firebase_creds["private_key_id"],  # You might need to extract this from your firebase_creds
-#     "private_key": firebase_creds["private_key"],
-#     "client_email": firebase_creds["client_email"],
-#     "client_id": firebase_creds["client_id"],
-#     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-#     "token_uri": "https://oauth2.googleapis.com/token",
-#     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-#     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-yn14k%40berlin-feedback.iam.gserviceaccount.com"
-# })
 if not firebase_admin._apps:  # Ensure Firebase is not initialized multiple times
     cred = credentials.Certificate(dict(firebase_creds))
     firebase_admin.initialize_app(cred)
-
-# class FirebaseService:
-#     def __init__(self):
-#         load_dotenv()
-#         cred_path = os.getenv('FIREBASE_CREDENTIALS_PATH')
-#         if cred_path and not firebase_admin._apps:
-#             cred = credentials.Certificate(cred_path)
-#             firebase_admin.initialize_app(cred)
-#         self.db = firestore.client()
 
 class DataService:
     @st.cache_data
