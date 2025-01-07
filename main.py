8b91edc1d5afb5ba00124b650a2025aac18e13d6
Gaurav Pandey <gp141022@gmail.com>
2025-01-07 11:38:41 +0100

    Updated Code

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class DataService:
     @st.cache_data
     def load_data(_self):
         data = pd.read_csv("datasets/processed_data.csv")
+        data.rename(columns={"Breitengrad": "Latitude", "Längengrad": "Longitude"}, inplace=True)
         geo_data = pd.read_csv("datasets/geodata_berlin_plz.csv", delimiter=';')
         geo_data['plz'] = geo_data['PLZ'].astype(int)
         return data, geo_data
