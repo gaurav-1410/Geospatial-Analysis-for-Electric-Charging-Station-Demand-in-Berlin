5434b648cdf167353e188ffe112803af59256ce3
Gaurav Pandey <gp141022@gmail.com>
2025-01-10 09:43:18 +0100

    Updated Code

--- src/data_service.py
+++ src/data_service.py
@@ -1,17 +1,3 @@
-# import pandas as pd
-# from streamlit import cache_data
-# import streamlit as st
-
-# class DataService:
-#     @staticmethod
-#     @st.cache_data
-#     def load_data():
-#         data = pd.read_csv("../datasets/processed_data.csv")
-#         data.rename(columns={"Breitengrad": "Latitude", "Längengrad": "Longitude"}, inplace=True)
-#         geo_data = pd.read_csv("../datasets/geodata_berlin_plz.csv", delimiter=';')
-#         geo_data['plz'] = geo_data['PLZ'].astype(int)
-#         return data, geo_data
-
 import os
 import pandas as pd
 import streamlit as st
