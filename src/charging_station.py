c6ec11b2c2510f942edceec0dfa9e5754c847b26
Gaurav Pandey <gp141022@gmail.com>
2025-02-03 20:53:59 +0100

    Code_refactor_for_final_updation

--- src/charging_station.py
+++ src/charging_station.py
@@ -1,27 +1,14 @@
 from typing import List
 
 class ChargingStation:
-    def __init__(self, station_id: str, pincode: str, latitude: float, longitude: float) -> None:
-        """
-        Initializes a ChargingStation object with the given attributes.
-
-        :param station_id: The unique ID of the charging station.
-        :param pincode: The postal code where the station is located.
-        :param latitude: The latitude of the charging station's location.
-        :param longitude: The longitude of the charging station's location.
-        """
-        self.station_id: str = station_id
-        self.pincode: str = pincode
-        self.latitude: float = latitude
-        self.longitude: float = longitude
-        self.feedbacks: List[str] = []
-
-    def get_station_id(self) -> str:
-        """
-        Returns the unique station ID of the charging station.
-
-        :return: The station ID.
-        """
+    def __init__(self, station_id, postal_code, latitude, longitude):
+        self.station_id = station_id
+        self.postal_code = postal_code
+        self.latitude = latitude
+        self.longitude = longitude
+        self.feedbacks = []
+    
+    def get_station_id(self):
         return self.station_id
 
     def add_feedback(self, feedback: str) -> None:
