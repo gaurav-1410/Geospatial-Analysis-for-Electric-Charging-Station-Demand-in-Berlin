43990a24f291705f056db354f1b2c49b3eb12996
Gaurav Pandey <gp141022@gmail.com>
2025-02-03 19:03:25 +0100

    Code_refactor

--- src/map_service.py
+++ src/map_service.py
@@ -92,7 +92,7 @@ class MapService:
                 areas[postal_code] = Area(postal_code)
 
             station = ChargingStation(
-                station_id=MapService.station_id_counter,
+                station_id=str(MapService.station_id_counter),
                 postal_code=postal_code,
                 latitude=latitude,
                 longitude=longitude
